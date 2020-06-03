86953b234e228487b33eadcf908a59762be968d4
Alex Kim <agkim@lbl.gov>
2020-06-03 16:10:27 -0700

    include galaxy redshift too

--- src/optimal.py
+++ src/optimal.py
@@ -26,26 +26,32 @@ class Fisher(object):
 
     def fmatrix(self):
         N = len(sigmas)
-        F = numpy.zeros((3+N,3+N))
+        F = numpy.zeros((4+N,4+N))
 
         F[0,0] = (self.invsigma2*self.tanh**2).sum()
         for i in range(N):
             F[0,1+i]=self.v_inf/self.a * self.invsigma2[i]*self.tanh[i]*self.sech2[i]
         F[0,N+1] = - self.v_inf/self.a*(self.invsigma2*self.tanh*self.sech2).sum()
         F[0,N+2] = - self.v_inf/self.a**2*((self.theta_i-self.theta_0)*self.invsigma2*self.tanh*self.sech2).sum()
+        F[0,N+3] = (self.invsigma2*self.tanh).sum()
 
         for i in range(N):
           F[1+i,1+i]=self.v_inf**2/self.a**2 * self.invsigma2[i]*self.sech2[i]**2 + self.invsigmatheta2    
           F[1+i,N+1]= - self.v_inf**2/self.a**2* self.invsigma2[i] *self.sech2[i]**2
           F[1+i,N+2]= - self.v_inf**2/self.a**3 *(self.theta_i[i]-self.theta_0)* self.invsigma2[i] *self.sech2[i]**2
+          F[1+i,N+3]= self.v_inf/self.a * self.invsigma2[i]*self.sech2[i]
 
         F[N+1,N+1] = self.v_inf**2/self.a**2 * (self.invsigma2*self.sech2**2).sum()
         F[N+1,N+2] = self.v_inf**2/self.a**3*((self.theta_i-self.theta_0)*self.invsigma2*self.sech2**2).sum()
+        F[N+1,N+3] = - self.v_inf/self.a * (self.invsigma2*self.sech2).sum()
 
         F[N+2,N+2]= self.v_inf**2/self.a**4*((self.theta_i-self.theta_0)**2*self.invsigma2*self.sech2**2).sum()
+        F[N+2,N+3]= -self.v_inf**2/self.a**2*((self.theta_i-self.theta_0)*self.invsigma2*self.sech2).sum()
 
-        for i in range(0,3+N):
-          for j in range(i+1,3+N):
+        F[N+3,N+3] = self.invsigma2.sum()
+
+        for i in range(0,4+N):
+          for j in range(i+1,4+N):
             F[j,i]=F[i,j]
         return F
 
@@ -67,4 +73,4 @@ fm_inv = numpy.linalg.inv(fm)
 # wef
 print(fm_inv)
 print(numpy.diag(fm))
-print(numpy.diag(fm_inv))
+print(numpy.sqrt(numpy.diag(fm_inv)))
