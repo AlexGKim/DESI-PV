20358098213c929aca7657677ce0c95781e9d6d1
Alex Kim <agkim@lbl.gov>
2020-06-04 11:52:13 -0700

    works and consolidating flags on objective function

--- src/optimal.py
+++ src/optimal.py
@@ -1,76 +1,116 @@
 #!/usr/bin/env python
 import numpy.random
-import scipy.optimize
+from scipy.optimize import minimize
 import scipy.stats
 import matplotlib.pyplot as plt
 
 class Fisher(object):
 
-    def __init__(self,fiducial,sigmas,sigma_theta):
-        self.set_fiducial(fiducial)
-        self.invsigma2 = 1/sigmas**2
-        self.invsigmatheta2 = 1/sigma_theta**2
-        self.calcfids()
+  def __init__(self,fiducial,sigmas,sigma_theta):
+    self.set_fiducial(fiducial)
+    self.invsigma2 = 1/sigmas**2
+    self.invsigmatheta2 = 1/sigma_theta**2
+    self.calcfids()
 
-    def calcfids(self):
-        self.tanh = numpy.tanh((self.theta_i-self.theta_0)/self.a)
-        self.sech2 = 1-self.tanh**2
+  def calcfids(self):
+    self.tanh = numpy.tanh((self.theta_i-self.theta_0)/self.a)
+    self.sech2 = 1-self.tanh**2
 
-    def set_fiducial(self,fiducial):
-        self.fiducial=fiducial
-        self.v_inf=self.fiducial['v_inf']
-        self.theta_i=self.fiducial['theta_i']
-        self.theta_0=self.fiducial['theta_0']
-        self.a = self.fiducial['a']
-        self.calcfids()
+  def set_fiducial(self,fiducial):
+    self.fiducial=fiducial
+    self.v_inf=self.fiducial['v_inf']
+    self.theta_i=self.fiducial['theta_i']
+    self.theta_0=self.fiducial['theta_0']
+    self.a = self.fiducial['a']
+    self.calcfids()
 
-    def fmatrix(self):
-        N = len(sigmas)
-        F = numpy.zeros((4+N,4+N))
+  def set_sigmas(self,sigmas):
+    self.invsigma2 = 1/sigmas**2    
 
-        F[0,0] = (self.invsigma2*self.tanh**2).sum()
-        for i in range(N):
-            F[0,1+i]=self.v_inf/self.a * self.invsigma2[i]*self.tanh[i]*self.sech2[i]
-        F[0,N+1] = - self.v_inf/self.a*(self.invsigma2*self.tanh*self.sech2).sum()
-        F[0,N+2] = - self.v_inf/self.a**2*((self.theta_i-self.theta_0)*self.invsigma2*self.tanh*self.sech2).sum()
-        F[0,N+3] = (self.invsigma2*self.tanh).sum()
+  def fmatrix(self):
+    # print(self.theta_i)
 
-        for i in range(N):
-          F[1+i,1+i]=self.v_inf**2/self.a**2 * self.invsigma2[i]*self.sech2[i]**2 + self.invsigmatheta2    
-          F[1+i,N+1]= - self.v_inf**2/self.a**2* self.invsigma2[i] *self.sech2[i]**2
-          F[1+i,N+2]= - self.v_inf**2/self.a**3 *(self.theta_i[i]-self.theta_0)* self.invsigma2[i] *self.sech2[i]**2
-          F[1+i,N+3]= self.v_inf/self.a * self.invsigma2[i]*self.sech2[i]
+    N = len(self.invsigma2)
+    F = numpy.zeros((4+N,4+N))
 
-        F[N+1,N+1] = self.v_inf**2/self.a**2 * (self.invsigma2*self.sech2**2).sum()
-        F[N+1,N+2] = self.v_inf**2/self.a**3*((self.theta_i-self.theta_0)*self.invsigma2*self.sech2**2).sum()
-        F[N+1,N+3] = - self.v_inf/self.a * (self.invsigma2*self.sech2).sum()
+    F[0,0] = (self.invsigma2*self.tanh**2).sum()
+    for i in range(N):
+        F[0,1+i]=self.v_inf/self.a * self.invsigma2[i]*self.tanh[i]*self.sech2[i]
+    F[0,N+1] = - self.v_inf/self.a*(self.invsigma2*self.tanh*self.sech2).sum()
+    F[0,N+2] = - self.v_inf/self.a**2*((self.theta_i-self.theta_0)*self.invsigma2*self.tanh*self.sech2).sum()
+    F[0,N+3] = (self.invsigma2*self.tanh).sum()
 
-        F[N+2,N+2]= self.v_inf**2/self.a**4*((self.theta_i-self.theta_0)**2*self.invsigma2*self.sech2**2).sum()
-        F[N+2,N+3]= -self.v_inf**2/self.a**2*((self.theta_i-self.theta_0)*self.invsigma2*self.sech2).sum()
+    for i in range(N):
+      F[1+i,1+i]=self.v_inf**2/self.a**2 * self.invsigma2[i]*self.sech2[i]**2 + self.invsigmatheta2    
+      F[1+i,N+1]= - self.v_inf**2/self.a**2* self.invsigma2[i] *self.sech2[i]**2
+      F[1+i,N+2]= - self.v_inf**2/self.a**3 *(self.theta_i[i]-self.theta_0)* self.invsigma2[i] *self.sech2[i]**2
+      F[1+i,N+3]= self.v_inf/self.a * self.invsigma2[i]*self.sech2[i]
 
-        F[N+3,N+3] = self.invsigma2.sum()
+    F[N+1,N+1] = self.v_inf**2/self.a**2 * (self.invsigma2*self.sech2**2).sum()
+    F[N+1,N+2] = self.v_inf**2/self.a**3*((self.theta_i-self.theta_0)*self.invsigma2*self.sech2**2).sum()
+    F[N+1,N+3] = - self.v_inf/self.a * (self.invsigma2*self.sech2).sum()
 
-        for i in range(0,4+N):
-          for j in range(i+1,4+N):
-            F[j,i]=F[i,j]
-        return F
+    F[N+2,N+2]= self.v_inf**2/self.a**4*((self.theta_i-self.theta_0)**2*self.invsigma2*self.sech2**2).sum()
+    F[N+2,N+3]= -self.v_inf**2/self.a**2*((self.theta_i-self.theta_0)*self.invsigma2*self.sech2).sum()
 
-fiducial=dict()
-fiducial['v_inf']=1.
-fiducial['theta_i']=numpy.array([-1,-.5,0,.5,1])
-fiducial['theta_0']=0.
-fiducial['a']=0.75
+    F[N+3,N+3] = self.invsigma2.sum()
 
-sigmas=numpy.full(len(fiducial['theta_i']),0.02)
-sigmas_theta = 0.02
+    for i in range(0,4+N):
+      for j in range(i+1,4+N):
+        F[j,i]=F[i,j]
+    return F
 
-f  = Fisher(fiducial, sigmas, sigmas_theta)
-fm =f.fmatrix()
-fm_inv = numpy.linalg.inv(fm)
-# print(fm)
-# print(fm[1,:])
-# print(fm[3,:])
-# wef
-print(fm_inv)
-print(numpy.diag(fm))
-print(numpy.sqrt(numpy.diag(fm_inv)))
+
+def sersicProfile(fiducial, n=4.):
+  b=2*n-1./3
+  return numpy.exp(-b*((numpy.abs(fiducial['theta_i']-fiducial['theta_0'])/fiducial['a'])**(1./n) -1))
+
+def sigmas_v(norm, background, fiducial, profile, **kwargs):
+  gflux = profile(fiducial, **kwargs)
+  return norm* numpy.sqrt(background + gflux)/gflux
+
+def x_to_theta(x):
+  x[1:] = numpy.exp(x[1:])
+  theta_i = numpy.array(x)
+  for i in range(1,len(x)):
+    theta_i[i]=theta_i[i-1]+x[i]
+  return theta_i  
+
+def objective(x, fisher,index=0):
+
+  theta_i = x_to_theta(x)
+
+  fiducial = fisher.fiducial
+  fiducial['theta_i'] = theta_i
+
+  fisher.set_fiducial(fiducial)
+
+  fisher.set_sigmas(sigmas_v(0.1, 0, fiducial, sersicProfile))
+  f = fisher.fmatrix()
+  ans = numpy.sqrt(numpy.linalg.inv(f)[index,index])
+  return ans
+
+def main():
+
+  # values of the fiducial parameters
+  fiducial=dict()
+  fiducial['v_inf']=1.
+  fiducial['theta_i']=numpy.array([-1,-0.5,.5,1])
+  fiducial['theta_0']=0.
+  fiducial['a']=0.75
+
+  #values of the measurement errors
+  sigmas = sigmas_v(0.1, 0, fiducial, sersicProfile)
+  sigmas_theta = 0.02
+
+  f  = Fisher(fiducial, sigmas, sigmas_theta)
+
+  x0=numpy.full(len(fiducial['theta_i']),-1)
+  res = minimize(objective, x0, method='powell',args=(f), options={'xatol': 1e-8, 'disp': False})
+  print(x_to_theta(res.x))
+  res = minimize(objective, x0, method='powell',args=(f,-1), options={'xatol': 1e-8, 'disp': False})
+  print(x_to_theta(res.x))
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
