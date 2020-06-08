652c5b71a2fbb64256267145079a07920410ec38
Alex Kim <agkim@lbl.gov>
2020-06-08 13:38:18 -0700

    refactoring code

--- src/optimal.py
+++ src/optimal.py
@@ -60,15 +60,57 @@ class Fisher(object):
         F[j,i]=F[i,j]
     return F
 
+# The noise based on signal to noise so depends on
+#
+# profile :     shape of the galaxy
+# backgound:    background flux
+# 
+# The noise is normalized based on the
+# sigma_v_max:  the maximum possible noise
+# theta_max:    the maximum radius that has the maximum noise
 
-def sersicProfile(fiducial, n=1.):
+class MaxNoiseModel(object):
+
+  def __init__(self,theta_max, sigma_v_max, background, profile, **kwargs):
+    self.theta_max = theta_max
+    self.sigma_v_max = sigma_v_max
+    self.background = background
+    self.profile = profile
+    if kwargs:
+      self.kwargs = kwargs
+    else:
+      self.kwargs = dict()
+
+  def sigmas(self, theta):
+    minflux = self.profile(self.theta_max, **self.kwargs)
+    ans = numpy.zeros(len(theta))
+    w = numpy.abs(theta) < self.theta_max
+    gflux = self.profile(theta[w], **self.kwargs)
+    ans[w] = numpy.sqrt((gflux+self.background)/(minflux+self.background)) /gflux * minflux
+    w = numpy.abs(theta) >= self.theta_max
+    ans[w] = numpy.exp(5*(numpy.abs(theta[w]) - self.theta_max))
+    ans = self.sigma_v_max*ans
+    return ans
+
+  @staticmethod
+  def test():
+    sigma_v_max=10.
+    theta = numpy.arange(-4.1,4.101,0.01)
+    theta_max=4
+    background=1
+    sig = MaxNoiseModel(theta_max, sigma_v_max, background, sersicProfile)
+    sigs = sig.sigmas(theta)
+    # plt.plot(theta, sig.sigmas(theta))
+    sig.background=0
+    sigs = sigs/sig.sigmas(theta)
+    plt.plot(theta, sigs)
+    plt.show()
+
+def sersicProfile(theta_Re, n=1.):
   b=2*n-1./3
-  return numpy.exp(-b*((numpy.abs(fiducial['theta_i']-fiducial['theta_0'])/fiducial['a'])**(1./n) -1))
+  return numpy.exp(-b*(numpy.abs(theta_Re)**(1./n) -1))
 
-def sigmas_v(norm, background, fiducial, profile, **kwargs):
-  gflux = profile(fiducial, **kwargs)
-  return norm* numpy.sqrt(background + gflux)/gflux
-
+# the objective function variable 'x' are not theta, the transformation between the two are
 def x_to_theta(x):
   x[1:] = numpy.exp(x[1:]/100)
   theta_i = numpy.array(x)
@@ -82,9 +124,9 @@ def theta_to_x(theta):
   x[1:]= 100*numpy.log(delta[:-1])
   return x
 
-def objective(x, fisher, profile, index=0, zero=False):
+def objective(x, fisher, noiseModel, index=0, addZero=False):
 
-  if zero:
+  if addZero:
     x=numpy.append(x,0)
 
   theta_i = x_to_theta(x)
@@ -94,7 +136,7 @@ def objective(x, fisher, profile, index=0, zero=False):
 
   fisher.set_fiducial(fiducial)
 
-  fisher.set_sigmas(sigmas_v(0.1, 0, fiducial, profile))
+  fisher.set_sigmas(noiseModel.sigmas(fiducial['theta_i']-fiducial['theta_0']))
   f = fisher.fmatrix()
   ans = numpy.sqrt(numpy.linalg.inv(f)[index,index])
   return ans
@@ -109,31 +151,31 @@ def main():
   fiducial['a']=1.
 
   #values of the measurement errors
-  sigmas = sigmas_v(0.1, 0, fiducial, sersicProfile)
+  # sigmas = sigmas_v(0.1, 0, fiducial, sersicProfile)
+  sigma_v_max=10.
+  theta_max=4
+  background=1
+  noiseModel = MaxNoiseModel(theta_max, sigma_v_max, background, sersicProfile)
+  # sigmas = sigmas_v(fiducial['theta_i']-fiducial['theta_0'], 2, 0.1, 0, sersicProfile)
+  sigmas = noiseModel.sigmas(fiducial['theta_i']-fiducial['theta_0'])
   sigmas_theta = 0.02
 
   f  = Fisher(fiducial, sigmas, sigmas_theta)
 
-
   x0=theta_to_x(numpy.array([-1,-0.5,.5,1]))
-  res = minimize(objective, x0, method='powell',args=(f, sersicProfile), options={'xtol': 1e-8, 'disp': False})
+  res = minimize(objective, x0, method='powell',args=(f, noiseModel), options={'xtol': 1e-8, 'disp': False})
   print(x_to_theta(res.x), res.fun)
 
   x0=theta_to_x(numpy.array([-1,-0.5,.5,1]))
-  res = minimize(objective, x0, method='powell',args=(f, sersicProfile,-1), options={'xtol': 1e-8, 'disp': False})
-  print(x_to_theta(res.x), res.fun)
-
-  # x0=theta_to_x(numpy.array([-1,-0.67,-0.33,0]))
-  # res = minimize(objective, x0, method='powell',args=(f,-1), options={'xtol': 1e-8, 'disp': False})
-  # print(x_to_theta(res.x), res.fun)
-  # wefwe
-
-  x0=theta_to_x(numpy.array([-1,-0.5,.5,1]))
-  res = minimize(objective, x0, method='powell',args=(f, sersicProfile,0,True), options={'xtol': 1e-8, 'disp': False})
+  res = minimize(objective, x0, method='powell',args=(f, noiseModel,-1), options={'xtol': 1e-8, 'disp': False})
   print(x_to_theta(res.x), res.fun)
 
   x0=theta_to_x(numpy.array([-1,-0.5,.5,1]))
-  res = minimize(objective, x0, method='powell',args=(f, sersicProfile,-1,True), options={'xtol': 1e-8, 'disp': False})
+  res = minimize(objective, x0, method='powell',args=(f, noiseModel,0,True), options={'xtol': 1e-8, 'disp': False})
+  print(x_to_theta(res.x), res.fun)
+
+  x0=theta_to_x(numpy.array([-1,-0.5,.5,1]))
+  res = minimize(objective, x0, method='powell',args=(f, noiseModel,-1,True), options={'xtol': 1e-8, 'disp': False})
   print(x_to_theta(res.x), res.fun)
 
 if __name__ == "__main__":
